d54cceabd6f3bf83c63e7f6b9deaf47391d3371e
Dez <desmondadkins@gmail.com>
2021-04-08 18:57:06 -0400

    useState on form setup

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { submitQuestion } from "../actions/question_actions";
 
 const Form = ({ submitQuestion }) => {
+  const [name, setName] = useState("");
+  const [text, setText] = useState("");
+  const [repo, setRepo] = useState("");
+  const [live, setLive] = useState("");
+
   return (
-    <>
-      <h1>Form</h1>
+    <input>
+      <input type="text" placeholder="name" onChange={() => setName()}>
+        {name}
+      </input>
+      <input type="text" placeholder="text" onChange={() => setText()}>
+        {text}
+      </input>
+      <input type="text" placeholder="repo" onChange={() => setRepo()}>
+        {repo}
+      </input>
+      <input type="text" placeholder="live" onChange={() => setLive()}>
+        {live}
+      </input>
     </>
   );
 };
