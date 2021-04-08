4b91e0e2015e75a172efdccda2b2599ddfc70ff5
Dez <desmondadkins@gmail.com>
2021-04-08 15:26:12 -0400

    get, post, delete setup

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const app = express();
 
 // Mongoose below
 const mongoose = require("mongoose");
+
+const questions = require("./routes/api/questions");
 const db = require("./config/keys").mongoURI;
+app.use(express.json());
 
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Mongo!!"))
   .catch((err) => console.log(err));
+
+app.use("api/questions", questions);
 
 app.get("/", (req, res) => res.send("Hello! Hola!"));
 
